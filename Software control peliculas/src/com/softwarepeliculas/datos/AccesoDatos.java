ee6465e78ad770c0d9ca245c6c8f3771d9922ee9
jmolano188 <jmolano188@unab.edu.co>
2022-11-15 20:08:47 -0500

    se creo la interfaz que contiene los metodos de acceso a datos

--- Software control peliculas/src/com/softwarepeliculas/datos/AccesoDatos.java
+++ Software control peliculas/src/com/softwarepeliculas/datos/AccesoDatos.java
@@ -1,4 +1,17 @@
 package com.softwarepeliculas.datos;
 
+import com.softwarepeliculas.domain.Pelicula;
+import com.softwarepeliculas.excepciones.AccesoDatosExcepcion;
+import com.softwarepeliculas.excepciones.EscrituraDatosExcepcion;
+import com.softwarepeliculas.excepciones.LecturaDatosExcepcion;
+
+import java.util.List;
+
 public interface AccesoDatos {
+    boolean comprobarSiExisteArchivo(String nombreArchivo) throws AccesoDatosExcepcion;
+    List<Pelicula> listar(Pelicula pelicula) throws LecturaDatosExcepcion;
+    public void escribir(Pelicula pelicula, String nombreArchivo,boolean anexar) throws EscrituraDatosExcepcion;
+    public String buscar(String nombreArchivo, String pelicula)throws LecturaDatosExcepcion;
+    public void crear(String nombreArchivo)throws  EscrituraDatosExcepcion;
+    public void borrar(String nombreArchivo) throws LecturaDatosExcepcion;
 }
